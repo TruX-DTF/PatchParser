f8c16ce1a4c3a59ac8fb09320c5179494ff99c05
Kui LIU <brucekuiliu@gmail.com>
2018-06-19 21:05:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ mvn package
 mv -f target/Parser-0.0.1-SNAPSHOT.jar target/dependency/
 cd ..
 
-java -cp "Parser/target/dependency/*" -Xmx1024g edu.lu.uni.serval.Main
-java -cp "Parser/target/dependency/*" -Xmx1024g edu.lu.uni.serval.Main2
+java -cp "Parser/target/dependency/*" -Xmx2g edu.lu.uni.serval.Main
+java -cp "Parser/target/dependency/*" -Xmx2g edu.lu.uni.serval.Main2
 
