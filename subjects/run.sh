8be36faaefaaf0a4584b72e9667a37203e8c29c5
Kui LIU <brucekuiliu@gmail.com>
2018-06-19 21:04:27 +0800

    Update run.sh

--- subjects/run.sh
+++ subjects/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+cd subjects
 git clone https://github.com/apache/commons-io.git
 git clone https://github.com/apache/commons-lang.git
 git clone https://github.com/apache/commons-math.git
 git clone https://github.com/apache/derby.git
 git clone https://github.com/apache/mahout.git
 git clone https://github.com/apache/lucene-solr.git
-
+cd ..
 
